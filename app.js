6da13ea79a2453325712767f7e9df53c07dd5bbe
Sam Holdsworth <sdholdsworth86@hotmail.com>
2019-01-23 22:16:46 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const val = 'test connection to remote GitHub repo attempt #1';
+
+// const button = document.getElementById('test-btn');
+document.querySelector('#test-btn').addEventListener('click', function(e) {
+
+    document.getElementById('test').innerHTML = 'The button was clicked and I was changed!'
+    console.log(val);
+});
+
+
+
